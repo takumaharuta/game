1e46b5335bb71ea49d621d4cfde32063917e0243
takumaharuta <apple.0201tk@gmail.com>
2024-08-24 13:05:56 +0900

    購入一覧をコンポーネント化できた

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -4,12 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\Purchase;
 
 class MypageController extends Controller
 {
     public function index()
     {
-        return Inertia::render('Mypage');
+        $purchasedWorks = Purchase::with('contentPage')
+            ->where('user_id', auth()->id())
+            ->orderBy('created_at', 'desc')
+            ->get()
+            ->map(function ($purchase) {
+                return [
+                    'id' => $purchase->contentPage->id,
+                    'title' => $purchase->contentPage->title,
+                    'cover_image' => $purchase->contentPage->cover_image,
+                    'author_name' => $purchase->contentPage->author_name,
+                    'purchase_date' => $purchase->created_at->format('Y-m-d'),
+                ];
+            });
+            
+        return Inertia::render('Mypage',[
+            'purchasedWorks' => $purchasedWorks
+        ]);
     }
 
     public function getUserInfo(Request $request)
